725d3a4e0d5d22ef2d33a78bdbb053c05de52f7c
Fillipe <fillipeolipe@gmail.com>
2022-03-02 14:57:06 -0300

    Colocado o modulo sharp e implementado a opção de redimencionar a img

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+const sharp = require('sharp');
+
 let path = process.argv[2];
 let width = Number(process.argv[3]);
+let new_name = process.argv[4];
 
-function resize(path, width) {}
+function resize(path, width, new_name) {
+	sharp(path)
+		.resize(width)
+		.toFile(`./temp/${new_name}.jpg`, (error) => {
+			if (error) throw error;
+		});
+}
+
+resize(path, width, new_name);
