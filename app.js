c8f42cff8c8f58f9f25277e90a4f557e9fe8a084
Fillipe <fillipeolipe@gmail.com>
2022-03-02 15:14:56 -0300

    Colocando o pacote compress-image e seus dependentes e criando a função para comprimir a imgaem

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const sharp = require('sharp');
+const compress_images = require('compress-images');
 
 let path = process.argv[2];
 let width = Number(process.argv[3]);
 let new_name = process.argv[4];
 
 function resize(path, width, new_name) {
+	let output_path = `./temp/${new_name}.jpg`;
 	sharp(path)
 		.resize(width)
-		.toFile(`./temp/${new_name}.jpg`, (error) => {
+		.toFile(output_path, (error) => {
 			if (error) throw error;
+			console.log(new_name);
+			compress(output_path, `./compressed/`);
+            
 		});
+}
+
+function compress(input_path, output_path) {
+	compress_images(
+		input_path,
+		output_path,
+		{ compress_force: false, statistic: true, autoupdate: true },
+		false,
+		{ jpg: { engine: 'mozjpeg', command: ['-quality', '60'] } },
+		{ png: { engine: 'pngquant', command: ['--quality=20-50', '-o'] } },
+		{ svg: { engine: 'svgo', command: '--multipass' } },
+		{
+			gif: {
+				engine: 'gifsicle',
+				command: ['--colors', '64', '--use-col=web'],
+			},
+		},
+		function (error, completed, statistic) {
+			console.log('-------------');
+			console.log(error);
+			console.log(completed);
+			console.log(statistic);
+			console.log('-------------');
+		}
+	);
 }
 
 resize(path, width, new_name);
